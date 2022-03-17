fd8cc8ad21979843ba0576444d4404f8e8d09083
Jia-Baos <18383827268@163.com>
2022-03-17 11:20:50 +0800

    2022-03-17, 11:20

--- Myfunctest.py
+++ Myfunctest.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 r_grid = np.array(list(range(14)))
 # 对数字进行重复，repeats：次数，axis：重复的维度
@@ -9,3 +10,8 @@ c_grid = np.array(list(range(14)))
 c_grid = np.repeat(c_grid, repeats=2, axis=0)[np.newaxis, :]    # [[0 0 1 1 2 2 3 3 4 4 5 5 6 6]]
 c_grid = np.repeat(c_grid, repeats=14, axis=0).reshape(-1)  # [0 0 1 1 2 2 3 3 4 4 5 5 6 6 0 0 1 1 2 2 3 3 4 4 5 5 6 6...]
 print(np.size(c_grid))
+
+input = torch.randint(low=0, high=10, size=(10, 1))
+print(input)
+input = torch.clamp(input, min=2)
+print(input)
